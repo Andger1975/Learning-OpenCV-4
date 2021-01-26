f9d52e2beee76012b3915e8b7db2b9805531ba87
Andger1975 <agerman@yandex.ru>
2021-01-26 18:52:45 +1000

    Office

--- appendix/cameo/cameo.py
+++ appendix/cameo/cameo.py
@@ -1,12 +1,13 @@
 import cv2
 from managers import PygameWindowManager as WindowManager, \
-                     CaptureManager
+    CaptureManager
+
 
 class Cameo(object):
 
     def __init__(self):
         self._windowManager = WindowManager('Cameo',
-                                             self.onKeypress)
+                                            self.onKeypress)
         self._captureManager = CaptureManager(
             cv2.VideoCapture(0), self._windowManager, True)
 
@@ -32,16 +33,17 @@ class Cameo(object):
         escape -> Quit.
 
         """
-        if keycode == 32: # space
+        if keycode == 32:  # space
             self._captureManager.writeImage('screenshot.png')
-        elif keycode == 9: # tab
+        elif keycode == 9:  # tab
             if not self._captureManager.isWritingVideo:
                 self._captureManager.startWritingVideo(
                     'screencast.avi')
             else:
                 self._captureManager.stopWritingVideo()
-        elif keycode == 27: # escape
+        elif keycode == 27:  # escape
             self._windowManager.destroyWindow()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     Cameo().run()
